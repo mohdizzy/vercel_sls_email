5b06a35bfa62838c83a57e50934b07f9c0c8894a
Mohammed <mohdizzy@gmail.com>
2020-07-22 19:29:43 +0530

    const

---
+++ pages/api/send-email.js
@@ -0,0 +1,41 @@
+AWS = require('aws-sdk'),
+SES = new AWS.SES({ region: "us-east-1" }),
+
+EMAIL = 'mohammed@serverlessguru.com',
+UTF8CHARSET = 'UTF-8';
+
+module.exports = async (req,res) => {
+
+    const emailData = JSON.parse(req);
+    console.log(req);
+
+    if (!emailData.name || !emailData.message || !emailData.email) {
+        res.status(400).send("Please fill all inputs");
+    }
+
+ 
+    const body = emailData.message;
+
+    const emailParams = {
+        Destination: EMAIL,
+        Message: {
+            Body: body,
+            Subject: {
+                Charset: UTF8CHARSET,
+                Data: `Email from ${emailData.name}`
+            }
+        },
+        Source: EMAIL
+    };
+
+
+    try {
+        await SES.sendEmail(emailParams).promise();
+        res.status(200);
+        res.send("Thank you providing your details");
+    } catch (err) {
+        console.error(err, err.stack);
+        res.status(400).send(err);
+    }
+};
+
